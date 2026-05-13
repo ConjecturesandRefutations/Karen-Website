0c9c3e68fdde44a5817a79101065882deddb5832
alfie collins <alfiecollins44@hotmail.co.uk>
2026-05-13 20:10:43 +0100

    improvements to the about page

--- src/data/projects.js
+++ src/data/projects.js
@@ -20,11 +20,11 @@ import visaFour from "../assets/images/Visa_Conexiones/Four.jpg";
 import visaFive from "../assets/images/Visa_Conexiones/Five.jpg";
 
 //Bajo la Espuma
-import bajoFeatured from "../assets/images/Bajo_la_Espuma/Featured"
+/* import bajoFeatured from "../assets/images/Bajo_la_Espuma/Featured"
 import bajoTwo from "../assets/images/Bajo_la_Espuma/Two.jpg";
 import bajoThree from "../assets/images/Bajo_la_Espuma/Three.jpg";
 import bajoFour from "../assets/images/Bajo_la_Espuma/Four.jpg";
-import bajoFive from "../assets/images/Bajo_la_Espuma/Five.jpg";
+import bajoFive from "../assets/images/Bajo_la_Espuma/Five.jpg"; */
 
 
 
