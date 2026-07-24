5d76cf15bd9470068944a29db0b685de12f24b64
alfie collins <alfiecollins44@hotmail.co.uk>
2026-07-24 18:26:32 +0100

    update to process photos

--- src/pages/Process.jsx
+++ src/pages/Process.jsx
@@ -6,6 +6,12 @@ import threea from "../assets/images/Process/3/3A.jpg";
 import threeb from "../assets/images/Process/3/3B.jpg";
 import foura from "../assets/images/Process/4/4A.JPEG";
 import fourb from "../assets/images/Process/4/4B.jpg";
+import fivea from "../assets/images/Process/5/5A.JPEG";
+import fiveb from "../assets/images/Process/5/5B.JPEG";
+import sixa from "../assets/images/Process/6/6A.JPG";
+import sixb from "../assets/images/Process/6/6B.jpg";
+import sevena from "../assets/images/Process/7/7A.png";
+import sevenb from "../assets/images/Process/7/7B.jpg";
 
 export default function About() {
   // Define the 9 grid slots. 4 are filled with your before/after images, 5 are placeholders.
@@ -14,9 +20,9 @@ export default function About() {
     { id: 2, isFilled: true, imgA: twoa, imgB: twob, alt: "Process 2" },
     { id: 3, isFilled: true, imgA: threea, imgB: threeb, alt: "Process 3" },
     { id: 4, isFilled: true, imgA: foura, imgB: fourb, alt: "Process 4" },
-    { id: 5, isFilled: false },
-    { id: 6, isFilled: false },
-    { id: 7, isFilled: false },
+    { id: 5, isFilled: true, imgA: fivea, imgB: fiveb, alt: "Process 5" },
+    { id: 6, isFilled: true, imgA: sixa, imgB: sixb, alt: "Process 6" },
+    { id: 7, isFilled: true, imgA: sevena, imgB: sevenb, alt: "Process 7"   },
     { id: 8, isFilled: false },
     { id: 9, isFilled: false },
   ];
